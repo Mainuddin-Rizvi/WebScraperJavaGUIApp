04f6733ad13a18d81a77159625a39410b78ac927
Mainuddin <web97development@gmail.com>
2023-02-07 23:37:25 +0530

    final completion

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,6 +5,10 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -29,9 +33,49 @@ public class App {
 
     }
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello world!");
-        Scanner sc = new Scanner(System.in);
-        String country = sc.nextLine();
-        System.out.println(getData(country));
+//        System.out.println("Hello world!");
+//        Scanner sc = new Scanner(System.in);
+//        String country = sc.nextLine();
+//        System.out.println(getData(country));
+
+
+
+//        creating GUI
+        JFrame root = new JFrame("Details of Country");
+        root.setSize(500,500);
+
+        Font f  = new Font("Poppins",Font.BOLD,30);
+
+        //text field
+        JTextField field = new JTextField();
+
+        //label
+        JLabel dataL = new JLabel();
+
+        field.setFont(f);
+        dataL.setFont(f);
+        field.setHorizontalAlignment(SwingConstants.CENTER);
+        dataL.setHorizontalAlignment(SwingConstants.CENTER);
+
+        //button
+        JButton button = new JButton("Get");
+        button.addActionListener(event -> {
+            try{
+                String maindata = field.getText();
+                String result = getData(maindata);
+                dataL.setText(result);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        button.setFont(f);
+        root.setLayout(new BorderLayout());
+        root.add(field, BorderLayout.NORTH);
+        root.add(dataL, BorderLayout.CENTER);
+        root.add(button, BorderLayout.SOUTH);
+        root.setVisible(true);
+
     }
 }
